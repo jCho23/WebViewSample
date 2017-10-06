21795fc449212b829d98af2dc26023cc5fb292f2
June Cho <junecho.ucb@gmail.com>
2017-10-06 11:26:18 -0700

    Intial Commit

---
+++ WebViewSample/WebViewSamplePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace WebViewSample
+{
+    public partial class WebViewSamplePage : ContentPage
+    {
+        public WebViewSamplePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
