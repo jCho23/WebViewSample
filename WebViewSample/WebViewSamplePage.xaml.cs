250a07c8e06ea9b6a3cbf7927e58664f78d4b21a
June Cho <junecho.ucb@gmail.com>
2017-10-06 11:55:50 -0700

    Added WebView

--- WebViewSample/WebViewSamplePage.xaml.cs
+++ WebViewSample/WebViewSamplePage.xaml.cs
@@ -5,8 +5,35 @@ namespace WebViewSample
     public partial class WebViewSamplePage : ContentPage
     {
         public WebViewSamplePage()
-        {
-            InitializeComponent();
-        }
-    }
+		{
+			Label header = new Label
+			{
+				Text = "WebView",
+				FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+				HorizontalOptions = LayoutOptions.Center
+			};
+
+			WebView webView = new WebView
+			{
+				Source = new UrlWebViewSource
+				{
+					Url = "http://blog.xamarin.com/",
+				},
+				VerticalOptions = LayoutOptions.FillAndExpand
+			};
+
+			// Accomodate iPhone status bar.
+			this.Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
+
+			// Build the page.
+			this.Content = new StackLayout
+			{
+				Children =
+				{
+					header,
+					webView
+				}
+			};
+		}
+	}
 }
